c85bb970a824e261aa5c3ad1a9300e0df60b5219
sonnguyen27204-code <sonnguyen27204@gmail.com>
2026-08-05 02:39:36 +0700

    Add webpack.mix.js

---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .options({
+        processCssUrls: false,
+    });
